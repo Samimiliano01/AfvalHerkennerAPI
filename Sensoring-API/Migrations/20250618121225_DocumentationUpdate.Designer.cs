466e4ee0a9b560c45cc44a625220be2bef69c66d
SamvNoort <samimiliano01@gmail.com>
2025-06-18 14:16:27 +0200

    Updated Migrations to include the comments cuz thats needed for some reason

--- Sensoring-API/Migrations/20250618121225_DocumentationUpdate.Designer.cs
+++ Sensoring-API/Migrations/20250618121225_DocumentationUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sensoring_API.Data;
 
@@ -11,9 +12,11 @@ using Sensoring_API.Data;
 namespace Sensoring_API.Migrations
 {
     [DbContext(typeof(LitterDbContext))]
-    partial class LitterDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250618121225_DocumentationUpdate")]
+    partial class DocumentationUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
